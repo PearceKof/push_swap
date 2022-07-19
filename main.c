c185584b0e7e9f41617febb9d3767ee3a1489f40
PearceKof <root@DESKTOP-0SM51MM.localdomain>
2022-07-19 17:59:57 +0200

    first

---
+++ main.c
@@ -0,0 +1,29 @@
+
+#include "push_swap.h"
+
+/*typedef struct s_stack
+{
+	int				nbr;
+	int				index;
+	int				pos;
+	int				target_pos;
+	int				cost_a;
+	int				cost_b;
+	struct s_stack	*next;
+}	t_stack;
+*/
+
+int	main(int ac, char **ag)
+{
+	t_stack	a;
+	t_stack	b;
+
+	if (ac < 2)
+	{
+		ft_putstr_fd("There is no argument", 2);
+		return (1);
+	}
+	else if (ac == 3)
+	else if (ac > 3)
+	return (0);
+}
---
+++ push_swap.h
@@ -0,0 +1,19 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct s_stack
+{
+	int				nbr;
+	int				index;
+	int				pos;
+	int				target_pos;
+	int				cost_a;
+	int				cost_b;
+	struct s_stack	*next;
+}	t_stack;
+
+#endif
